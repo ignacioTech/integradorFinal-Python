828782d6120fb030460920fd65c8b42f8fe96775
ignacioTech <ignaciobarboza1983@gmail.com>
2024-12-29 17:16:26 -0300

    Update index.py

--- index.py
+++ index.py
@@ -34,7 +34,7 @@ def registrar_producto():
      
     while True:
         try:
-            precio = float(input("Ingrese cantidad del producto: "))
+            cantidad = float(input("Ingrese cantidad del producto: "))
             if(precio >= 0):
                 break
             print("la cantidad debe ser mayor o igual a 0")
@@ -42,3 +42,43 @@ def registrar_producto():
             print("Entrada inválida, ingrese un valor mayor o igual que cero")
     
     categoria = input("Ingrese la categoría del producto").strip()
+
+    #Regisro de los productos detallados en el inventario/diccionario
+
+    inventario[f"Producto- {contador_index}"]={
+        "nombre" : nombre,
+        "descripcion" : descripcion,
+        "cantidad" : cantidad,
+        "precio" : precio,
+        "categoria" : categoria
+    }
+    print(f"el producto fue generado con exito, Codigo Asignado: Producto-{contador_index}")
+    contador_index += 1
+    
+def mostrar_productos():
+    print("\n Listado de productos")
+
+    if not inventario:
+        print("El inventario se encuentra vacío")
+    else:
+        for codigo, datos in inventario.items():
+            print(f"Codigo       : {codigo}")
+            print(f"Nombre       : {datos["nombre"]}")
+            print(f"descripcion  : {datos["descripcion"]}")
+            print(f"cantidad     : {datos["cantidad"]}")
+            print(f"precio       : {datos["precio"]}")
+            print(f"categoria    : {datos["categoria"]}")
+            print(f"- -"*50)
+            
+def actualizar_productos():
+    print("\n Actualizacion de productos")
+    codigo= input("Ingrese el código del producto a editar")
+
+    if codigo in inventario:
+        print(f"Producto encontrado: {inventario[codigo]["nombre"]}")
+        print("Ingrese los nuevos datos o enter para no modificar")
+
+        n_nombre=input("Ingrese el nuevo nombre")
+        n_descripcion= input("Ingrese la nueva descripcion")
+
+        
