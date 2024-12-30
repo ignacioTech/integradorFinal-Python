a1730a099d1123d18dd1f2555e010db5c61aa072
ignacioTech <ignaciobarboza1983@gmail.com>
2024-12-29 23:55:48 -0300

    Update index.py

--- index.py
+++ index.py
@@ -45,7 +45,7 @@ def registrar_producto():
 
     #Regisro de los productos detallados en el inventario/diccionario
 
-    inventario[f"Producto- {contador_index}"]={
+    inventario[f"{contador_index}"]={
         "nombre" : nombre,
         "descripcion" : descripcion,
         "cantidad" : cantidad,
@@ -80,5 +80,145 @@ def actualizar_productos():
 
         n_nombre=input("Ingrese el nuevo nombre")
         n_descripcion= input("Ingrese la nueva descripcion")
-
         
+        #validacion de nueva cantidad
+        while True :
+            n_cantidad= input("ingrese la nueva cantidad").strip()
+            if not n_cantidad :
+                break
+            try:
+                n_cantidad = int(n_cantidad)
+                if n_cantidad >= 0 :
+                    break
+                else:
+                    print("ingreso invalido, debe ser numerico positivo")
+            except ValueError:
+                print("el valor debe ser positivo")
+                
+        
+        #validacion de nuevo precio
+        while True :
+            n_precio= input("ingrese el nuevo precio").strip()
+            if not n_precio :
+                break
+            try:
+                n_precio = float(n_precio)
+                if n_precio >= 0 :
+                    break
+                else:
+                    print("ingreso invalido, debe ser numerico positivo")
+            except ValueError:
+                print("el valor debe ser positivo")
+        
+        n_categoria = input("Ingrese la nueva categoria")
+
+        #Ãctualizar datos
+        if n_nombre :
+            inventario[codigo]["nombre"]= n_nombre
+        if n_descripcion :
+            inventario[codigo]["descripcion"]= n_descripcion
+        if n_cantidad :
+            inventario[codigo]["cantidad"]= n_cantidad
+        if n_precio :
+            inventario[codigo]["precio"]= n_precio
+        if n_categoria :
+            inventario[codigo]["categoria"]= n_categoria
+            
+        print("Producto actualizado")
+    else:
+        print("El codigo no existe")
+
+def eliminar_producto():
+    print("\n Eliminar Producto")
+    codigo = input("Ingrese el codigo a eliminar").strip()
+
+    if codigo in inventario:
+        del inventario[codigo]
+        print(f"El codigo: {codigo} , se ha eliminado")
+    else:
+        print(f"El codigo {codigo} no existe")
+
+def buscar_producto():
+    print("\n Buscar Producto")
+    codigo = input("Ingrese el codigo a buscar").strip()
+
+    if codigo in inventario:
+        producto = inventario[codigo]
+        print(f"\n Informacion del producto encontrado con el codigo{codigo}")
+
+        print(f"Nombre       : {producto["nombre"]}")
+        print(f"descripcion  : {producto["descripcion"]}")
+        print(f"cantidad     : {producto["cantidad"]}")
+        print(f"precio       : {producto["precio"]}")
+        print(f"categoria    : {producto["categoria"]}")
+            
+    else:
+        print(f"El codigo {codigo} no existe")
+        
+def reporte_bajo_stock():
+    print("\n Reporte bajo stock")
+
+    while True: 
+        try:
+            limite= int(input("Ingrese le limite de Stock para generar el reporte"))
+            if limite >=0 :
+                break
+            else:
+                print("Ingrese un numero positivo")
+        except ValueError:
+            print("Ingrese un numero positivo")
+    
+    print(f"\n productos con stock inferior a  {limite}")
+
+    prod_bajo_stock=[
+        (codigo, datos) for codigo, datos in inventario.items()
+            if datos["cantidad"] <= limite
+    ]
+    
+    if not prod_bajo_stock:
+        print("No hay productos con bajo stock")
+    
+    for codigo, datos in prod_bajo_stock.items():
+            print(f"Codigo       : {codigo}")
+            print(f"Nombre       : {datos["nombre"]}")
+            print(f"descripcion  : {datos["descripcion"]}")
+            print(f"cantidad     : {datos["cantidad"]}")
+            print(f"precio       : {datos["precio"]}")
+            print(f"categoria    : {datos["categoria"]}")
+            print(f"- -"*50)
+            
+#EJECUCION PROGRAMA PRINCIPAL
+while True:  
+    mostrar_menu()
+    
+    try:
+        opcion = int(input("Ingrese una opción del 1 - 6 || 0(cero) para salir "))
+
+        if opcion == 0:
+            print("Salir")
+            break
+
+        elif opcion == 1:
+           registrar_producto()
+
+        elif opcion == 2:
+            mostrar_productos()
+            
+        elif opcion == 3:
+            print("3-Actualizacion-")
+            actualizar_productos()
+            
+        elif opcion == 4:
+            print("4-Eliminacion-")
+            eliminar_producto()
+            
+        elif opcion == 5:
+            print("5-Listado-")
+            buscar_producto()
+        elif opcion == 6:
+            print("6-Reporte Bajo Stock-")
+            reporte_bajo_stock()
+        else:
+            print("Ingrese un valor valido entre  1 - 6 || 0(cero) para salir ")
+    except ValueError:
+        print("ingrese un valor numerico valido")
